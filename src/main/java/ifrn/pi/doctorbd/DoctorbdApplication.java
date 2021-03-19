4f11d3ba6ab82ee5355fbb390ac38dd4189bc58a
NatanController <natangab007@gmail.com>
2021-03-19 08:53:25 -0300

    Colocando arquivos iniciais

---
+++ src/main/java/ifrn/pi/doctorbd/DoctorbdApplication.java
@@ -0,0 +1,13 @@
+package ifrn.pi.doctorbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifrn/pi/doctorbd/DoctorbdApplicationTests.java
@@ -0,0 +1,13 @@
+package ifrn.pi.doctorbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

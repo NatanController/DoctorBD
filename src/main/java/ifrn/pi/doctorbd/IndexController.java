5bb2060a55ee4c81e798dec3b51f9622a60a14c9
NatanController <natangab007@gmail.com>
2021-03-19 09:50:19 -0300

    Atualizando Index

--- src/main/java/ifrn/pi/doctorbd/IndexController.java
+++ src/main/java/ifrn/pi/doctorbd/IndexController.java
@@ -1,5 +1,38 @@
 package ifrn.pi.doctorbd;
 
-public class IndexController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class IndexController { 
+	
+	@RequestMapping("/")
+	public String index(){
+		
+		return "login";
+}
+	
+	@RequestMapping("/cadastro")
+	public String pagcadastro(){
+		
+		return "cadastro";
+} 
+	
+	@RequestMapping("/cadastroUsuario")
+	public String pagcadastroUsuario(){
+		
+		return "cadastroUsuario";
+}
+	@RequestMapping("/cadastroCasaSaude")
+	public String pagcadastroCasaSaude() {
+		return "cadastroCasaSaude";
+	}
+	
+	@RequestMapping("/cadastroMedico")
+	public String pagcadastroMedico(){
+		
+		return "cadastroMedico";
+}
+
+	
 }
